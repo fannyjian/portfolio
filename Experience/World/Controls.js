84dbc9d14481e5d00b838dc713a2cc399b9057be
fannyjian <fannyj@u.nus.edu>
2023-06-27 14:42:50 +0800

    minor changes

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import Experience from "../Experience";
 
@@ -10,10 +11,22 @@ export default class Controls {
         this.camera = this.experience.camera;
         this.resources = this.experience.resources;
         this.time = this.experience.time;
+        this.prince = this.experience.world.prince.actualPrince;
+        GSAP.registerPlugin(ScrollTrigger);
 
+        this.setPath();
     }
 
-    resize() {
+    setPath(){
+        console.log(this.prince);
+        this.timeline = new GSAP.timeline();
+        this.timeline.to(this.prince.position, {
+            x: 5,
+        });
+    }
+
+    resize() { 
+
     }
 
 
