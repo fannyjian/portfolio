7f110c06647e040854d1146c5663e11690f9642b
fannyjian <fannyj@u.nus.edu>
2023-07-11 13:52:10 +0800

    edit renderer

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -22,8 +22,6 @@ export default class Renderer {
         this.renderer.outputColorSpace = THREE.SRGBColorSpace;
         this.renderer.toneMapping = THREE.CineonToneMapping;
         this.renderer.toneMappingExposure = 1.75;
-        this.renderer.shadowMap.enabled = true;
-        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         this.renderer.setSize(this.sizes.width, this.sizes.height);
         this.renderer.setPixelRatio(this.sizes.pixelRatio);
     }
