766b5ab4d037324e20299d9efd954451aa1e146a
fannyjian <fannyj@u.nus.edu>
2023-07-11 13:22:01 +0800

    remove perspective cam

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -10,20 +10,9 @@ export default class Camera {
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
 
-        // 2 cameras - second for pathing
-        this.createPerspectiveCamera();
         this.createOrthographicCamera();
-        this.setOrbitControls();
     }
 
-    createPerspectiveCamera() {
-        this.perspectiveCamera = new THREE.PerspectiveCamera(35, this.sizes.aspect, 0.1, 1000);
-        this.scene.add(this.perspectiveCamera)
-        this.perspectiveCamera.position.x=29;
-        this.perspectiveCamera.position.y=14;
-        this.perspectiveCamera.position.z=12;
-
-    }
 
     createOrthographicCamera() {
         this.orthographicCamera = new THREE.OrthographicCamera(
@@ -44,32 +33,9 @@ export default class Camera {
 
 
         this.scene.add(this.orthographicCamera);
-
-        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
-        // this.scene.add(this.helper);
-
-        const size = 20;
-        const divisions = 20;
-
-        // const gridHelper = new THREE.GridHelper(size, divisions);
-        // this.scene.add(gridHelper);
-
-        // const axesHelper = new THREE.AxesHelper(10);
-        // this.scene.add(axesHelper);
-    }
-
-    setOrbitControls() {
-        this.controls = new OrbitControls(this.perspectiveCamera, this.canvas)
-        this.controls.enableDamping = true;
-        this.controls.enableZoom = false;
     }
 
     resize() {
-        // updating perspective cam on resize 
-        this.perspectiveCamera.aspect = this.sizes.aspect
-        this.perspectiveCamera.updateProjectionMatrix()
-
-        // updating orthographic cam on resize 
         this.orthographicCamera.left =
             (-this.sizes.aspect * this.sizes.frustrum) / 2;
         this.orthographicCamera.right =
@@ -80,11 +46,6 @@ export default class Camera {
     }
 
     update() {
-        this.controls.update();
 
-        // this.helper.matrixWorldNeedsUpdate = true;
-        // this.helper.update();
-        // this.helper.position.copy(this.orthographicCamera.position);
-        // this.helper.position.copy(this.orthographicCamera.rotation);
     }
 }
