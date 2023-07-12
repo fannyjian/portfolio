6967d696efdbe0662b21349db51a1d7b2e7239e2
fannyjian <fannyj@u.nus.edu>
2023-07-12 14:41:07 +0800

    change toggle bar

--- Experience/Theme.js
+++ Experience/Theme.js
@@ -8,6 +8,7 @@ export default class Theme extends EventEmitter {
 
         this.toggleButton = document.querySelector(".toggle-button")
         this.toggleCircle = document.querySelector(".toggle-circle")
+        this.toggleImg = document.querySelector(".toggle-image")
 
         this.setEventListeners();
     }
@@ -15,6 +16,7 @@ export default class Theme extends EventEmitter {
     setEventListeners() {
         this.toggleButton.addEventListener("click", () => {
             this.toggleCircle.classList.toggle("slide");
+            this.toggleImg.src = this.theme === "dark" ? "./images/sun.png" : "./images/night.png";
             this.theme = this.theme === "dark" ? "light" : "dark";
             document.body.classList.toggle("light-theme");
 
